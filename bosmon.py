90937664bbde2bc94e5f99b50e31e503e4d38d6d
timniklas <accounts@tnkoch.de>
2024-12-02 20:01:00 +0100

    improved loggin

--- bosmon.py
+++ bosmon.py
@@ -13,12 +13,12 @@ ENDPOINT=sys.argv[2]
 USERNAME=sys.argv[3]
 PASSWORD=sys.argv[4]
 
-async def publish(addr: str, func: str):
+async def publish(addr: str, func: str, type: str = 'pocsag'):
         try:
                 async with aiohttp.ClientSession() as websession:
                         async with websession.post(f'{URL}/telegramin/{ENDPOINT}/input.xml', data={
-                                'type': 'pocsag',
-                                'address': addr.rjust(7, '0'),
+                                'type': type,
+                                'address': addr,
                                 'function': func
                         }, auth=aiohttp.BasicAuth(USERNAME, PASSWORD)) as response:
                                 response.raise_for_status()
@@ -32,7 +32,7 @@ for line in sys.stdin:
                 break
         result = re.search(pattern, line)
         if result:
-                addr = result[1]
+                addr = result[1].rjust(7, '0')
                 key = result[2]
                 func = VALUES[int(key)]
                 local_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
